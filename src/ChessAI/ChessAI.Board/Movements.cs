c074e1835ad7b28bafff552b8fa9589ad5095e5e
Carlos M. Hernández <cmhernandezdel@gmail.com>
2023-10-23 23:24:29 +0200

    Make movements extension methods for ulong for chaining.

--- src/ChessAI/ChessAI.Board/Movements.cs
+++ src/ChessAI/ChessAI.Board/Movements.cs
@@ -8,22 +8,22 @@ public static class Movements
     private const ulong hFileMask = 0x8080808080808080;
 
     // Move from xN to x(N+1) where x is a file (A-H) and N is a rank (1-8)
-    public static ulong UpOneRank(ulong board) => board << 8;
+    public static ulong UpOneRank(this ulong board) => board << 8;
 
     // Move from xN to x(N-1) where x is a file (A-H) and N is a rank (1-8)
-    public static ulong DownOneRank(ulong board) => board >> 8;
+    public static ulong DownOneRank(this ulong board) => board >> 8;
 
     // Move from xN to (x+1)N where x is a file (A-H) and N is a rank (1-8)
-    public static ulong UpOneFile(ulong board) => (board << 1) & ~aFileMask;
+    public static ulong UpOneFile(this ulong board) => (board << 1) & ~aFileMask;
 
     // Move from xN to (x-1)N where x is a file (A-H) and N is a rank (1-8)
-    public static ulong DownOneFile(ulong board) => (board >> 1) & ~hFileMask;
+    public static ulong DownOneFile(this ulong board) => (board >> 1) & ~hFileMask;
 
-    public static ulong UpOneRankUpOneFile(ulong board) => (board << 9) & ~aFileMask;
+    public static ulong UpOneRankUpOneFile(this ulong board) => (board << 9) & ~aFileMask;
 
-    public static ulong DownOneRankUpOneFile(ulong board) => (board >> 7) & ~aFileMask;
+    public static ulong DownOneRankUpOneFile(this ulong board) => (board >> 7) & ~aFileMask;
 
-    public static ulong UpOneRankDownOneFile(ulong board) => (board << 7) & ~hFileMask;
+    public static ulong UpOneRankDownOneFile(this ulong board) => (board << 7) & ~hFileMask;
 
-    public static ulong DownOneRankDownOneFile(ulong board) => (board >> 9) & ~hFileMask;
+    public static ulong DownOneRankDownOneFile(this ulong board) => (board >> 9) & ~hFileMask;
 }
