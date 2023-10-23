d57518926bfe730e273e8b03342b3f9842b58c8c
Carlos M. Hernández <cmhernandezdel@gmail.com>
2023-10-23 23:24:52 +0200

    Implement movements for white pawns.

---
+++ src/ChessAI/ChessAI.Board/Pieces/Pawn.cs
@@ -0,0 +1,16 @@
+﻿namespace ChessAI.Board.Pieces;
+
+public static class Pawn
+{
+    public static ulong GetWhiteSinglePushes(ulong startingPosition, ulong emptySquares)
+        => startingPosition.UpOneRank() & emptySquares;
+
+    public static ulong GetWhiteDoublePushes(ulong startingPosition, ulong emptySquares)
+        => GetWhiteSinglePushes(startingPosition, emptySquares).UpOneRank() & emptySquares & Board.RANK_4;
+
+    public static ulong GetWhitePawnCaptures(ulong whitePawns, ulong blackPieces)
+        => (whitePawns.UpOneRankUpOneFile() & blackPieces) | (whitePawns.UpOneRankDownOneFile() & blackPieces);
+
+    public static ulong GetWhitePawnPromotions(ulong whitePawns, ulong emptySquares, ulong blackPieces)
+        => (GetWhiteSinglePushes(whitePawns, emptySquares) | GetWhitePawnCaptures(whitePawns, blackPieces)) & Board.RANK_8;
+}
