578e5b1c9af1d4bab14c77c27a684dde5e5561fa
Carlos M. Hernández <cmhernandezdel@gmail.com>
2023-10-27 19:20:08 +0200

    Knight moves (not tested)

---
+++ src/ChessAI/ChessAI.Board/Pieces/Knight.cs
@@ -0,0 +1,18 @@
+﻿namespace ChessAI.Board.Pieces;
+
+public static class Knight
+{
+    // targets can be empty squares for quiet moves or opposing side pieces for captures
+    public static ulong GetMoves(ulong ownKnights, ulong targets, ulong ownPieces)
+    {
+        return 
+            ((ownKnights.UpOneFile().UpOneFile().UpOneRank() & targets) |
+            (ownKnights.UpOneFile().UpOneFile().DownOneRank() & targets) |
+            (ownKnights.UpOneFile().UpOneRank().UpOneRank() & targets) |
+            (ownKnights.UpOneFile().DownOneRank().DownOneRank() & targets) |
+            (ownKnights.DownOneFile().DownOneFile().UpOneRank() & targets) |
+            (ownKnights.DownOneFile().DownOneFile().DownOneRank() & targets) |
+            (ownKnights.DownOneFile().UpOneRank().UpOneRank() & targets) |
+            (ownKnights.DownOneFile().DownOneRank().DownOneRank() & targets)) & ~ownPieces;
+    }
+}
