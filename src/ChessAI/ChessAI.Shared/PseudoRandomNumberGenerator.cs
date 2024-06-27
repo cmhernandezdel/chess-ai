fc596901327e1c5e7ef2f60e7ad698966d42b449
cmhernandezdel <cmhernandezdel@gmail.com>
2024-06-27 21:37:02 +0200

    Add pseudo random number generator with xor shift algorithm.

---
+++ src/ChessAI/ChessAI.Shared/PseudoRandomNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace ChessAI.Shared;
+
+// See: https://en.wikipedia.org/wiki/Xorshift
+public sealed class PseudoRandomNumberGenerator
+{
+    private uint Seed = 1804289383;
+
+    public uint Next()
+    {
+        var number = Seed;
+        number ^= number << 13;
+        number ^= number >> 17;
+        number ^= number << 5;
+        Seed = number;
+        return number;
+    }
+}
