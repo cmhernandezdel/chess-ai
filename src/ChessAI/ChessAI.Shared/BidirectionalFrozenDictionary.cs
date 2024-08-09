2393f4587ba49b8476b614389fafc1dda5bf95ea
cmhernandezdel <cmhernandezdel@gmail.com>
2024-08-10 00:34:46 +0200

    Add support for a bidirectional frozen dictionary for ASCII piece representation.

---
+++ src/ChessAI/ChessAI.Shared/BidirectionalFrozenDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Frozen;
+
+namespace ChessAI.Shared;
+
+public sealed class BidirectionalFrozenDictionary<T1, T2> 
+    where T1 : notnull 
+    where T2 : notnull
+{
+    private readonly FrozenDictionary<T1, T2> _dict1;
+    private readonly FrozenDictionary<T2, T1> _dict2;
+
+    public BidirectionalFrozenDictionary(IEnumerable<(T1, T2)> source)
+    {
+        _dict1 = source.ToFrozenDictionary(t => t.Item1, t => t.Item2);
+        _dict2 = source.ToFrozenDictionary(t => t.Item2, t => t.Item1);
+    }
+
+    public T2 GetValue(T1 key)
+    {
+        return _dict1[key];
+    }
+
+    public T1 GetValue(T2 key)
+    {
+        return _dict2[key];
+    }
+}
